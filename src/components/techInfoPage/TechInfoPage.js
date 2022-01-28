b3500a36f866b82b9c84adcd0075ad53a6b670fa
SaduRavi <sadurshan.20200596@iit.ac.lk>
2022-01-28 17:15:31 +0530

    Structured Tech Page

--- src/components/techInfoPage/TechInfoPage.js
+++ src/components/techInfoPage/TechInfoPage.js
@@ -1,24 +1,18 @@
 import React, { useState } from 'react';
-import Common from '../login/common/Common';
 import Button from '@mui/material/Button';
+import { useNavigate } from "react-router";
 
 import './TechInfoPage.css';
 import Frontend from './frontend/Frontend';
 import Backend  from './backend/Backend';
 import Database from './database/Database';
 
-import { useNavigate } from "react-router";
-
 export default function TechInfoPage(){
   const navigate = useNavigate();
-
-  document.title="stack4u/TechnologyInformation";
-
-  const [backToSignup, setBackToSignup] = useState(false);
-  const toggleBackToSignup = () => setBackToSignup(true);
+  document.title="stack4u-TechnologyInformation";
 
   const [num, setNum] = useState(0);
-  const update = () => {
+  const switchForward = () => {
     if(num === 2){
       setNum(0);
     }
@@ -26,11 +20,14 @@ export default function TechInfoPage(){
       setNum(num + 1);
     } 
   }
-
-  if (backToSignup){
-    return <Common/>
+  const switchBackward = () => {
+    if(num === 0){
+      setNum(2);
+    }
+    else{
+      setNum(num - 1);
+    } 
   }
-
   function SwitchPage(){
     if (num === 0) {
       return <Frontend/>
@@ -43,10 +40,19 @@ export default function TechInfoPage(){
     }
   }
   return (
-    <div>
-      <Button variant="contained" onClick={() => navigate("../login/common/Common")} className = "backToLoginButton">BACK TO SIGN UP</Button>
-      <button onClick={update}> Click </button>
-      <SwitchPage/>
+    <div className="body">
+      <div className='backToLoginButton'>
+        <Button variant="contained" onClick={() => navigate("../login/common/Common")}>BACK TO SIGN UP</Button>
+      </div> 
+      <h2 style={{textAlign: "center"}}> TECHNOLOGY INFORMATION</h2>
+      <div style={{marginLeft:70}}>
+        <Button variant="contained" onClick={switchBackward}> { '<< Switch'} </Button>
+        <Button variant="contained" onClick={switchForward} style={{marginLeft:1000}}> { 'Switch >>'} </Button>
+      </div>
+      <div className='techContainer'>
+        <SwitchPage/>
+      </div>
+ 
     </div>
   )
 }
