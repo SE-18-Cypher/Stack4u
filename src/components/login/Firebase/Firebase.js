89e02b9011c3410e6e0ef92fb7ec06f5cd8dd933
ThisaruWicks <thisaru99wickra@gmail.com>
2022-02-06 21:38:55 +0530

    firebase disaster

--- src/components/login/Firebase/Firebase.js
+++ src/components/login/Firebase/Firebase.js
@@ -1,18 +1,8 @@
-import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from '../../../Firebase-config';
+import { useNavigate } from "react-router";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBvPnnU7yq28bOQbkrpR2T2YP8zlpm6AaI",
-  authDomain: "sign-in-4c024.firebaseapp.com",
-  projectId: "sign-in-4c024",
-  storageBucket: "sign-in-4c024.appspot.com",
-  messagingSenderId: "673328316656",
-  appId: "1:673328316656:web:ff6fd928ecdfd9c61e4235"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export const signInWithGoogle = () => {
@@ -21,10 +11,8 @@ export const signInWithGoogle = () => {
       const name = result.user.displayName;
       const email = result.user.email;
       const profilePic = result.user.photoURL;
-
-      localStorage.setItem("name", name);
-      localStorage.setItem("email", email);
-      localStorage.setItem("profilePic", profilePic);
+      const navigate = useNavigate();
+      navigate('./home')
     })
     .catch((error) => {
       console.log(error);
