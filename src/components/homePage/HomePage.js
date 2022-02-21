1ebc69988a517af81b6d0b772153bff778bb3bd2
SaduRavi <sadurshan.20200596@iit.ac.lk>
2022-02-21 23:11:22 +0530

    Added the remaining designs

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -3,11 +3,14 @@ import { FileUploader } from "react-drag-drop-files";
 import Button from '@mui/material/Button';
 
 import './HomePage.css';
-import mainPageComputer from './../../resources/images/mainpageComputer.png';
-import mainPageImageIcon from './../../resources/images/mainpageImageIcon.png';
-import mainpageRecommendationProcess from './../../resources/images/mainpageRecommendationProcess.png';
-import mainpageFooter from './../../resources/images/mainpageFooter.png';
-import mainpageFooterStack4uLogo from './../../resources/images/mainpageFooterStack4uLogo.png';
+import mainPageComputer from './../../resources/images/mainPage/mainpageComputer.png';
+import mainPageImageIcon from './../../resources/images/mainPage/mainpageImageIcon.png';
+import mainpageRecommendationProcess from './../../resources/images/mainPage/mainpageRecommendationProcess.png';
+import mainpageFooter from './../../resources/images/mainPage/mainpageFooter.png';
+import mainpageFooterStack4uLogo from './../../resources/images/mainPage/mainpageFooterStack4uLogo.png';
+import mainpageBackground from './../../resources/images/mainPage/mainpageBackground.png';
+import mainpageLaptop from './../../resources/images/mainPage/mainPageLaptop.png';
+
 import { useNavigate } from "react-router";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -32,38 +35,62 @@ export default function HomePage() {
     console.log(file);
 
     const storage = getStorage(app);
-    
+
     React.useEffect(() => {
-      uploadFiles();
+        uploadFiles();
     }, [file])
-    
+
     const uploadFiles = () => {
         console.log("Works")
         if (!file) return;
         const sotrageRef = ref(storage, `files/${file.name}`);
         const uploadTask = uploadBytesResumable(sotrageRef, file);
-    
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {
-            const prog = Math.round(
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            );
-          },
-          (error) => console.log(error),
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              console.log("File available at", downloadURL);
-            });
-          }
-        );
-      };
 
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const prog = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+            },
+            (error) => console.log(error),
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log("File available at", downloadURL);
+                });
+            }
+        );
+    };
+
+    
+    const testRef = React.useRef(null);
+    const scrollToElement = () => testRef.current.scrollIntoView();
     return (
         <div>
             <div className='commonbg' />
             <div className='mainPageContent'>
                 <NavBar />
+                <div style={{ color: 'white' }}>
+                    <img src={mainpageBackground} alt='decoration background' className='mainPageBackground' />
+                    <div className="content-left">
+                        <p className='mainPageBackgroundHeader'>Technology Stack <br /> Recommender</p>
+                        <p className='mainPageBackgroundText'>
+                            We present an online technology stack recommendation
+                            system to recommend the most suitable technology stack
+                            according to your given requiments.
+                            Upload the software requiment specification document
+                            or input the details manually, click on submit and get the
+                            recommended technology stack.
+                        </p>
+                        <div className='mainPageGetStartedButton'>
+                            <Button variant='contained' onClick={scrollToElement}> < span style={{ fontSize: 25 }}> Get Started</span> </Button>
+                        </div>
+                    </div>
+                    <div className="content-right2">
+                        <img src={mainpageLaptop} alt='decoration background' />
+                    </div>
+                </div>
+                <br ref={testRef}/>
                 <div className='dragAndDropOutsideContainer'>
                     <br />
                     <div className='dragAndDropContainer'>
@@ -95,7 +122,7 @@ export default function HomePage() {
                     <div style={{ backgroundColor: '#045794C9' }}>
                         <img src={mainpageFooter} alt='decoration - circuitboard' className='footerImage' />
                     </div>
-                    <div className="footerContent-left">
+                    <div className="content-left">
                         <img src={mainpageFooterStack4uLogo} alt='stack4u logo' />
                         <h6 >We present a web based technology stack <br /> recommendation system</h6>
                         <br />
@@ -107,7 +134,7 @@ export default function HomePage() {
                             <TwitterIcon />
                         </div>
                     </div>
-                    <div className="footerContent-right">
+                    <div className="content-right">
                         <h3> Contact us</h3> <br />
                         <h6> Phone : +94 77 559 5632</h6>
                         <h6> Email : cypherstack4u@gmail.com</h6>
