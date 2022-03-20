01879ec4decef4fbcb6ee39969ba00e7400d7b02
SaduRavi <sadurshan.20200596@iit.ac.lk>
2022-03-20 22:29:07 +0530

    Updated Redirect.js

--- src/components/presentationPage/Redirect.js
+++ src/components/presentationPage/Redirect.js
@@ -7,15 +7,18 @@ import Home from './../homePage/HomePage';
 export default function Redirect() {
     const [view, setView] = useState(0);
 
-    sessionStorage.setItem("user", null);
-    sessionStorage.setItem("guser", null);
-    sessionStorage.setItem("guserFirstName", null);
-    sessionStorage.setItem("guserSecondName", null);
+    // sessionStorage.setItem("user", null);
+    // sessionStorage.setItem("guser", null);
+    console.log(sessionStorage.getItem("guser"))
+    // sessionStorage.setItem("guserFirstName", null);
+    // sessionStorage.setItem("guserSecondName", null);
     const loggedInUser = localStorage.getItem("user");
     const loggedInUserg = localStorage.getItem("guser");
-    
-    console.log(loggedInUser)
-    console.log(loggedInUserg)
+    const ssloggedInUser = sessionStorage.getItem("user");
+    const ssloggedInUserg = sessionStorage.getItem("guser");
+
+    console.log(ssloggedInUser)
+    console.log(ssloggedInUserg)
     setTimeout(function () {
         if (view < 3) {
             setView(view + 1);
@@ -26,11 +29,11 @@ export default function Redirect() {
         if (view < 3) {
             return <PresentationPage />
         }
-        else if (loggedInUser === 'null' || loggedInUser === null) {
-            return <Common />
-        }
-        else {
+        if (ssloggedInUserg !== null &&  ssloggedInUser !== null) {
             return <Home />
+        }
+        if (loggedInUser === 'null' || loggedInUser === null) {
+            return <Common />
         }
     }
     return (
