f4442aaa003bf4ca8c0b00f5797cc59f35c0cfaf
Bupanie <bupaniechamathka@gmail.com>
2022-03-24 18:55:33 +0530

    Added the sign up functionality

--- src/components/login/signup/Signup.js
+++ src/components/login/signup/Signup.js
@@ -13,17 +13,21 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from "react-router";
 import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
 import app from '../../../Firebase-config';
+import { addDoc, collection, doc, getDoc, setDoc,onSnapshot, query, updateDoc } from "firebase/firestore";
+import { getFirestore } from "@firebase/firestore";
 
 const theme = createTheme();
 
 export default function Signup() {
-
     const navigate = useNavigate();
     document.title = "stack4u/SignUp";
     const provider = new GoogleAuthProvider();
 
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
+    
+    const [firstName, setFirstName] = React.useState('');
+    const [lastName, setLastName] = React.useState('');
 
     const checkAuth = (data) => {
         if (data.get('password').length < 7) {
@@ -77,13 +81,15 @@ export default function Signup() {
         const data = new FormData(event.currentTarget);
         checkAuth(data);
     };
-    
-    function signUpWithEmail() {
-        const auth = getAuth();
+
+    const signUpWithEmail = async () =>{
+        const auth = getAuth(app);
         createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                // const user = userCredential.user;
+            .then((res) => {
+                const user = res.user;
+                console.log(res);
+                console.log(user);
+                addUser(user);
                 navigate("/techinput");
             })
             .catch((error) => {
@@ -97,13 +103,37 @@ export default function Signup() {
             .then((result) => {
                 localStorage.setItem("user", result.user.uid);
                 localStorage.setItem("guser", result.user.photoURL);
-                console.log(result.user.photoURL)
+                let text = result.user.displayName;
+                const nameArray = text.split(" ");
+                const user = result.user;
+                addGoogleUser(user,nameArray[0],nameArray[1]);
                 navigate("/techinput");
             })
             .catch((error) => {
                 console.log(error);
             });
     }
+
+    const database = getFirestore(app);
+    const addUser = async (user) => {
+        const ref = doc(database, 'Users', user.uid);
+        await setDoc(ref, {
+            userID    : user.uid,
+            firstName : firstName,
+            lastName  : lastName,
+            email     : user.email
+        });
+    };
+
+    const addGoogleUser = async (user,fname,lname) => {
+        const ref = doc(database, 'Users', user.uid);
+        await setDoc(ref, {
+            userID    : user.uid,
+            firstName : fname,
+            lastName  : lname,
+            email     : user.email
+        });
+    };
 
     const [validFName, setValidFName] = React.useState(true);
     const [fNameHelpertext, setFNameHelpertext] = React.useState("");
@@ -153,6 +183,8 @@ export default function Signup() {
                                         fullWidth
                                         id="firstName"
                                         label={firstNameLabelName}
+                                        value={firstName}
+                                        onChange={(e) => setFirstName(e.target.value)}
                                         autoFocus
                                     />
                                 </Grid>
@@ -164,6 +196,8 @@ export default function Signup() {
                                         fullWidth
                                         id="lastName"
                                         label={lastNameLabelName}
+                                        value={lastName}
+                                        onChange={(e) => setLastName(e.target.value)}
                                         name="lastName"
                                         autoComplete="family-name"
                                     />
