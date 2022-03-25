909050a3b8ff6dd824ba02f59443cc8c47425c95
ThisaruWicks <thisaru99wickra@gmail.com>
2022-03-25 11:28:15 +0530

    progress on the questionnaire page

--- src/components/questionnaire/Questionnaire.js
+++ src/components/questionnaire/Questionnaire.js
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import './Questionnaire.css';
+import astro from '../../resources/images/output.png'
 
 const steps = [
     {
@@ -46,59 +47,62 @@ export default function VerticalLinearStepper() {
     };
 
     return (
-        <div className='bg'>
-            <Paper 
-            className='paper'
-            elevation={4}
-            
-            >
-                <Box sx={{ maxWidth: 400 }}>
-                    <Stepper activeStep={activeStep} orientation="vertical">
-                        {steps.map((step, index) => (
-                            <Step key={step.label}>
-                                <StepLabel
-                                    optional={
-                                        index === 2 ? (
-                                            <Typography variant="caption">Last step</Typography>
-                                        ) : null
-                                    }
-                                >
-                                    {step.label}
-                                </StepLabel>
-                                <StepContent>
-                                    <Typography>{step.description}</Typography>
-                                    <Box sx={{ mb: 2 }}>
-                                        <div>
-                                            <Button
-                                                variant="contained"
-                                                onClick={handleNext}
-                                                sx={{ mt: 1, mr: 1 }}
-                                            >
-                                                {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                                            </Button>
-                                            <Button
-                                                disabled={index === 0}
-                                                onClick={handleBack}
-                                                sx={{ mt: 1, mr: 1 }}
-                                            >
-                                                Back
-                                            </Button>
-                                        </div>
-                                    </Box>
-                                </StepContent>
-                            </Step>
-                        ))}
-                    </Stepper>
-                    {activeStep === steps.length && (
-                        <Paper square elevation={0} sx={{ p: 3 }}>
-                            <Typography>All steps completed - you&apos;re finished</Typography>
-                            <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-                                Reset
-                            </Button>
-                        </Paper>
-                    )}
-                </Box>
-            </Paper>
+        <div className='divM'>
+            <div className='divP'>
+                <Paper
+                    className='paper'
+                    elevation={4}
+                >
+                    <Box sx={{ maxWidth: 1000 }}>
+                        <Stepper activeStep={activeStep} orientation="vertical" >
+                            {steps.map((step, index) => (
+                                <Step key={step.label}>
+                                    <StepLabel
+                                        optional={
+                                            index === 2 ? (
+                                                <Typography variant="caption">Last step</Typography>
+                                            ) : null
+                                        }
+                                    >
+                                        {step.label}
+                                    </StepLabel>
+                                    <StepContent>
+                                        <Typography>{step.description}</Typography>
+                                        <Box sx={{ mb: 2 }}>
+                                            <div>
+                                                <Button
+                                                    variant="contained"
+                                                    onClick={handleNext}
+                                                    sx={{ mt: 1, mr: 1 }}
+                                                >
+                                                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                                                </Button>
+                                                <Button
+                                                    disabled={index === 0}
+                                                    onClick={handleBack}
+                                                    sx={{ mt: 1, mr: 1 }}
+                                                >
+                                                    Back
+                                                </Button>
+                                            </div>
+                                        </Box>
+                                    </StepContent>
+                                </Step>
+                            ))}
+                            
+                        </Stepper>
+                        {activeStep === steps.length && (
+                            <Paper square elevation={0} sx={{ p: 3 }}>
+                                <Typography>All steps completed - you&apos;re finished</Typography>
+                                <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                                    Reset
+                                </Button>
+                            </Paper>
+                        )}
+                    </Box>
+                </Paper>
+                <img id='pht' src={astro} style={{ height: '40%', width: '20%' }} />
+            </div>
         </div>
     );
 }
