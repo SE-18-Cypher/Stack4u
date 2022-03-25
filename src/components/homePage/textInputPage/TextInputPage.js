85f101a757f45d21235e67a2162efbcd2879483d
chamidiPerera <chaminihansa@gmail.com>
2022-03-25 12:11:41 +0530

    manual input page partially done

--- src/components/homePage/textInputPage/TextInputPage.js
+++ src/components/homePage/textInputPage/TextInputPage.js
@@ -3,67 +3,80 @@ import React from 'react'
 import { useNavigate } from 'react-router';
 import "./TextInputPage.css";
 import MenuItem from '@mui/material/MenuItem';
-
+export default function BasicButtons() {
 export default function TextInputPage() {
   const user = sessionStorage.getItem("user");
   const navigate = useNavigate();
 
-    React.useEffect(() => {
-        if (user === null) {
-            navigate('/access_error')
-        }
-    },)
-    const [userinput, setuserinput] = React.useState("")
+  React.useEffect(() => {
+    if (user === null) {
+      navigate('/access_error')
+    }
+  })
+  const [userinput, setuserinput] = React.useState("")
 
-    const currencies = [
-      {
-        value: 'USD',
-        label: '$',
-      },
-      {
-        value: 'EUR',
-        label: '€',
-      },
-      {
-        value: 'BTC',
-        label: '฿',
-      },
-      {
-        value: 'JPY',
-        label: '¥',
-      },
-    ];
-    const [currency, setCurrency] = React.useState('EUR');
+  const currencies = [
+    {
+      value: 'USD',
+      label: 'Mobile application',
+    },
+    {
+      value: 'EUR',
+      label: 'Web application',
+    },
+    {
+      value: 'BTC',
+      label: 'Both',
+    },
 
-    const handleChange = (event) => {
-      setCurrency(event.target.value);
-    };    
+  ];
+  const [currency, setCurrency] = React.useState('EUR');
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
   return (
     <div className='textinput'>
-      <TextField
+      <div className='description'>
+        <TextField
           id="filled-multiline-static"
           label="Please Enter The Requirement Specification"
-          fullWidth 
+          fullWidth
           multiline
           rows={10}
-          defaultValue={userinput} onChange = {(e)=>e.target.value}
+          defaultValue={userinput} onChange={(e) => e.target.value}
           variant="filled"
         />
+      </div>
 
-        <TextField
+      <div className='select'>
+        <TextField 
           id="outlined-select-currency"
           select
           label="Select"
           value={currency}
           onChange={handleChange}
-          helperText="Please select your currency"
-          >
+          helperText="Please select your platform"
+        >
           {currencies.map((option) => (
             <MenuItem key={option.value} value={option.value}>
               {option.label}
             </MenuItem>
           ))}
         </TextField>
-    </div> 
+
+        import Button from '@mui/material/Button';
+
+
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
+      </div>
+    </div>
   )
 }
