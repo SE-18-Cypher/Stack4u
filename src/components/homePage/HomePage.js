73b65fb826d4b10ec6e1b953ecd62f6f960c5138
SaduRavi <sadurshan.20200596@iit.ac.lk>
2022-03-26 21:44:36 +0530

    OCR for images

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -16,16 +16,17 @@ import fblogo from '../../resources/images/facebook.png';
 import app from './../../Firebase-config';
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { getStorage } from "firebase/storage";
+import Tesseract from 'tesseract.js';
 
-const fileTypes = ["JPEG", "PDF"];
+const fileTypes = ["JPEG", "PDF", "JPG", "PNG"];
 
 export default function HomePage() {
     const localUser = localStorage.getItem("user");
     const user = sessionStorage.getItem("user");
     const guser = sessionStorage.getItem("guser");
-    console.log(guser)
-    const guserFN = sessionStorage.getItem("guserFirstName");
-    const guserLN = sessionStorage.getItem("guserSecondName");
+    // console.log(guser)
+    // const guserFN = sessionStorage.getItem("guserFirstName");
+    // const guserLN = sessionStorage.getItem("guserSecondName");
     const navigate = useNavigate();
 
     React.useEffect(() => {
@@ -68,34 +69,67 @@ export default function HomePage() {
     const storage = getStorage(app);
 
     React.useEffect(() => {
-        uploadFiles();
+        handleSubmit();
     }, [file])
 
-    const uploadFiles = () => {
-        if (!file) return;
-        const sotrageRef = ref(storage, `files/${file.name}`);
-        const uploadTask = uploadBytesResumable(sotrageRef, file);
+    // const uploadFiles = () => {
+    //     if (!file) return;
+    //     const sotrageRef = ref(storage, `files/${file.name}`);
+    //     const uploadTask = uploadBytesResumable(sotrageRef, file);
 
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const prog = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-            },
-            (error) => console.log(error),
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                    console.log("File available at", downloadURL);
-                });
-            }
-        );
-    };
+    //     uploadTask.on(
+    //         "state_changed",
+    //         (snapshot) => {
+    //             const prog = Math.round(
+    //                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+    //             );
+    //         },
+    //         (error) => console.log(error),
+    //         () => {
+    //             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+    //                 console.log("File available at", downloadURL);
+    //             });
 
+    //         }
+    //     );
+    // };
 
     const testRef = React.useRef(null);
     const scrollToElement = () => testRef.current.scrollIntoView();
     console.log(user)
+
+    const [image, setImage] = React.useState('');
+    const [text, setText] = React.useState('');
+
+    const handleSubmit = () => {
+        var fileName = file.name
+        console.log(fileName === undefined)
+        if (fileName !== undefined) {
+            var fileExtension = fileName.split('.').pop();
+            console.log(fileExtension)
+            if (fileExtension === 'pdf') {
+                console.log("w")
+            }
+            else {
+                extractText(file);
+            }
+        }
+    };
+
+    function extractText(file) {
+        Tesseract.recognize(file, 'eng',
+            {
+                logger: m => console.log(m)
+            }
+        )
+            .catch((err) => {
+                console.error(err);
+            })
+            .then((result) => {
+                console.log(result.data.text);
+                setText(result.data.text);
+            });
+    }
     return (
 
         <div className='bk'>
