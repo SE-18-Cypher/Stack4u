adf61fda482ca081e5aea6e64896c3e583b328e9
ThisaruWicks <thisaru99wickra@gmail.com>
2022-03-30 12:01:32 +0530

    flask 2nd commit

--- src/components/homePage/textInputPage/TextInputPage.js
+++ src/components/homePage/textInputPage/TextInputPage.js
@@ -5,8 +5,9 @@ import "./TextInputPage.css";
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import bg from "./../../../resources/images/computerImage.png";
-    
-    
+import axios from "axios";
+
+
 
 export default function TextInputPage() {
   const user = sessionStorage.getItem("user");
@@ -39,51 +40,73 @@ export default function TextInputPage() {
   const handleChange = (event) => {
     setCurrency(event.target.value);
   };
+
+
+
+  function getInput() {
+    axios({
+      method: "POST",
+      url: "/input",
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+
+
   return (
     <div className="bground" style={{ backgroundImage: `url(${bg})` }}>
-    <div className='textinput'>
-      <div className='description'>
-        <TextField
-          id="filled-multiline-static"
-          label="Please Enter The Requirement Specification"
-          fullWidth
-          multiline
-          rows={30}
-          defaultValue={userinput} onChange={(e) => e.target.value}
-          variant="filled"
-          
-        />
+      <div className='textinput'>
+        <form action = "/input" method="POST">
+          <div className='description'>
+
+            <TextField
+              id="filled-multiline-static"
+              name="input"
+              label="Please Enter The Requirement Specification"
+              fullWidth
+              multiline
+              rows={30}
+              defaultValue={userinput} onChange={(e) => e.target.value}
+              variant="filled"
+
+            />
+          </div>
+
+          <div className='select'>
+            <TextField
+              className='opotions'
+              id="outlined-select-currency"
+              select
+              label="Select"
+              value={currency}
+              onChange={handleChange}
+              helperText="Please select the application you want"
+
+
+            >
+              {currencies.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+
+
+
+
+          </div>
+          <div className='button'>
+            <Button type="submit" variant="contained" size="small">
+              Submit
+            </Button></div>
+        </form>
       </div>
-
-      <div className='select'>
-        <TextField 
-          className='opotions'
-          id="outlined-select-currency"
-          select
-          label="Select"
-          value={currency}
-          onChange={handleChange}
-          helperText="Please select the application you want"
-          
-
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-
-        
-        
-
-      </div>
-      <div className='button'>
-        <Button variant="outlined" size="small">
-          Small
-        </Button></div>
-      
-    </div>
     </div>
   )
 }
